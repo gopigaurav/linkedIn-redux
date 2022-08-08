e476074d4ea47f55b9c3490655584a70c3d0bf04
gopi <48626860+gopigaurav@users.noreply.github.com>
2022-08-08 19:32:24 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBFEziu4_WGDNwgm2xf3vdKVz-SvjNNsas",
-  authDomain: "linkedin-clone-72ac6.firebaseapp.com",
-  projectId: "linkedin-clone-72ac6",
-  storageBucket: "linkedin-clone-72ac6.appspot.com",
-  messagingSenderId: "989846420955",
-  appId: "1:989846420955:web:e17621af77126f2f303bfe",
-  measurementId: "G-CFHGCB17ZW"
 };
 
 
